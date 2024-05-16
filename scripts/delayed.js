03361df7e3e0699038289af09fb07a1fb93092d2
szymon-zajaczkowski <szymon.zajaczkowski@ibmix.at>
2024-05-16 09:56:17 +0200

    Added 3td party JS

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,44 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './aem.js';
+import { sampleRUM, loadScript } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+loadScript('https://cdn.cookielaw.org/consent/c2736e32-7189-4085-9348-8f5a3e6f262a/OtAutoBlock.js', { type: 'application/x-javascript' });
+loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', { type: 'application/javascript' });
+loadScript('https://www.google.com/recaptcha/api.js?render=6LcaypMUAAAAAJrx_5YOYfgrbdQjcr7FPeENclgV');
+loadScript('https://assets.adobedtm.com/85168268b75b/f6da3cbfc19f/launch-ENe3a7b070e1c44212ba0e768f628e51d6.min.js', { type: 'application/x-javascript' });
+loadScript('https://www.lot.com/clientlibs/data-layer.min.ACSHASHa390ef0ab597e1376b9a3d2df5f5a4f6.js', { type: 'application/javascript' });
+loadScript('https://assets.adobedtm.com/extensions/EPc7341b33570d4c988798fc9f0093d4b2/AppMeasurement.min.js', { type: 'application/x-javascript' });
+loadScript('https://assets.adobedtm.com/extensions/EPc7341b33570d4c988798fc9f0093d4b2/AppMeasurement_Module_ActivityMap.min.js', { type: 'application/x-javascript' });
+loadScript('https://geolocation.onetrust.com/cookieconsentpub/v1/geo/location', { type: 'application/json' });
+loadScript('https://www.lot.com/clientlibs/amplify/amplify-production.min.ACSHASH0c71db12864f7ea7ace7aed981ebd94e.js', { type: 'application/javascript' });
+loadScript('https://www.gstatic.com/recaptcha/releases/vjbW55W42X033PfTdVf6Ft4q/recaptcha__en.js', { type: 'text/javascript' });
+loadScript('https://cdn.cookielaw.org/scripttemplates/202208.1.0/otBannerSdk.js', { type: 'application/javascript' });
+loadScript('https://www.clarity.ms/tag/h5cqcjcvf6', { type: 'application/javascript' });
+loadScript('https://assets.adobedtm.com/85168268b75b/f6da3cbfc19f/a2cbedbbbcf9/RCce4678ff507646508fc67ac9cb8472db-source.min.js', { type: 'application/x-javascript' });
+loadScript('https://assets.adobedtm.com/85168268b75b/f6da3cbfc19f/a2cbedbbbcf9/RC3b958aa7a48b470392652c64d5880ee1-source.min.js', { type: 'application/x-javascript' });
+loadScript('https://bat.bing.com/bat.js', { type: 'application/javascript' });
+loadScript('https://dpm.demdex.net/id?d_visid_ver=5.5.0&d_fieldgroup=AAM&d_rtbd=json&d_ver=2&d_orgid=35E8153D5645B5B97F000101%40AdobeOrg&d_nsid=0&d_mid=12418413528483038160053102405409759261&d_blob=6G1ynYcLPuiQxYZrsz_pkqfLG9yMXBpb2zX5dvJdYQJzPXImdj0y&ts=1715845350701', { type: 'application/json' });
+loadScript('https://www.googletagmanager.com/gtag/js?id=G-ZKDKKFHG23&l=customLayer', { type: 'application/javascript' });
+loadScript('https://bat.bing.com/p/action/343007013.js', { type: 'application/javascript' });
+loadScript('https://cdn.cookielaw.org/scripttemplates/202208.1.0/assets/otCookieSettingsButton.json', { type: 'application/json' });
+loadScript('https://bat.bing.com/p/insights/t/343007013', { type: 'application/x-javascript' });
+loadScript('https://www.clarity.ms/s/0.7.34/clarity.js', { type: 'application/javascript' });
+loadScript('https://www.googletagmanager.com/gtag/js?id=DC-12306264&l=customLayer', { type: 'application/javascript' });
+loadScript('https://www.googletagmanager.com/gtag/js?id=DC-12306264&l=customLayer&cx=c', { type: 'application/javascript' });
+loadScript('https://www.dwin1.com/10333.js', { type: 'application/javascript' });
+loadScript('https://www.lot.com/_sec/cp_challenge/sec-cpt-4-4.js', { type: 'application/javascript' });
+loadScript('https://www.gstatic.com/recaptcha/releases/vjbW55W42X033PfTdVf6Ft4q/recaptcha__en.js', { type: 'text/javascript' });
+loadScript('https://www.google.com/js/bg/EghGp72f3L_y9HVk4Nn8Vk_BAMWB9fGIEzP3DzvQDW8.js', { type: 'text/javascript' });
+loadScript('https://assets.adobedtm.com/85168268b75b/f6da3cbfc19f/a2cbedbbbcf9/RC785a8d505ffc4fe2aa466c756c864846-source.min.js', { type: 'text/x-javascript' });
+loadScript('https://assets.adobedtm.com/85168268b75b/f6da3cbfc19f/a2cbedbbbcf9/RC080b2da7c7a742e894e813442f0fa60e-source.min.js', { type: 'text/x-javascript' });
+loadScript('https://assets.adobedtm.com/85168268b75b/f6da3cbfc19f/a2cbedbbbcf9/RC5242236ddeae448e89b4aa6736767f1e-source.min.js', { type: 'text/x-javascript' });
+loadScript('https://assets.adobedtm.com/85168268b75b/f6da3cbfc19f/a2cbedbbbcf9/RC8aff35976da94312829af19266f33a2b-source.min.js', { type: 'text/x-javascript' });
+loadScript('https://assets.adobedtm.com/85168268b75b/f6da3cbfc19f/a2cbedbbbcf9/RC7b78bcd7cc13452e986e27c7dc53637f-source.min.js', { type: 'text/x-javascript' });
+loadScript('https://assets.adobedtm.com/85168268b75b/f6da3cbfc19f/a2cbedbbbcf9/RC10a5cffa0785490a963088cecd30c509-source.min.js', { type: 'text/javascript' });
+loadScript('https://assets.adobedtm.com/85168268b75b/f6da3cbfc19f/a2cbedbbbcf9/RCfdc64427ce234338ac3459ff5bafc803-source.min.js', { type: 'text/javascript' });
+loadScript('https://connect.facebook.net/en_US/fbevents.js', { type: 'text/javascript' });
+loadScript('https://www.googletagmanager.com/gtag/destination?id=AW-977250585&l=customLayer&cx=c', { type: 'application/javascript' });
+loadScript('https://connect.facebook.net/signals/config/1146883136325212?v=2.9.156&r=stable&domain=www.lot.com&hme=c3a545c63044e8e9102d4f32d84a1137594d024f28e801d670bc76dc5c075575&ex_m=67%2C112%2C99%2C103%2C58%2C3%2C93%2C66%2C15%2C91%2C84%2C49%2C51%2C158%2C161%2C172%2C168%2C169%2C171%2C28%2C94%2C50%2C73%2C170%2C153%2C156%2C165%2C166%2C173%2C121%2C14%2C48%2C178%2C177%2C123%2C17%2C33%2C38%2C1%2C41%2C62%2C63%2C64%2C68%2C88%2C16%2C13%2C90%2C87%2C86%2C100%2C102%2C37%2C101%2C29%2C25%2C154%2C157%2C130%2C27%2C10%2C11%2C12%2C5%2C6%2C24%2C21%2C22%2C54%2C59%2C61%2C71%2C95%2C26%2C72%2C8%2C7%2C76%2C46%2C20%2C97%2C96%2C9%2C19%2C18%2C81%2C53%2C79%2C32%2C70%2C0%2C89%2C31%2C78%2C83%2C45%2C44%2C82%2C36%2C4%2C85%2C77%2C42%2C39%2C34%2C80%2C2%2C35%2C60%2C40%2C98%2C43%2C75%2C65%2C104%2C57%2C56%2C30%2C92%2C55%2C52%2C47%2C74%2C69%2C23%2C105%2C184%2C183%2C185%2C190%2C191%2C192%2C188%2C180%2C122%2C150%2C179%2C181%2C113%2C144%2C135%2C139%2C119%2C174%2C216%2C106%2C217%2C152%2C110%2C133%2C126%2C114', { type: 'application/x-javascript' });
